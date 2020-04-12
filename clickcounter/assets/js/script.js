ba634d64f5c7f364ce7411e1ef8b6cb6c9bfc4a5
Yadnesh Khode <yadneshkhode@gmail.com>
2020-04-12 16:15:59 +0530

    optimized code

--- clickcounter/assets/js/script.js
+++ clickcounter/assets/js/script.js
@@ -1,60 +1,36 @@
-// var count=0;
-// var lowerbtn = document.querySelector(".lower-count");
-// lowerbtn.addEventListener("click",()=>{
-//     if(count > 0){
-//         count--;
-//         document.querySelector(".counter p").innerText = count;
-//     }
-//     else{
-//         lowerbtn.disabled = true;
-//         lowerbtn.classList.add("opacButton");
-//     }
-    
-// });
-// document.querySelector(".add-count").addEventListener("click",()=>{
-//     if(count>0){
-//         lowerbtn.disabled = false;
-//         lowerbtn.classList.remove("opacButton");
-//     }
-//     count++;
-//     document.querySelector(".counter p").innerText = count;
-// });
-
-
-// document.querySelector(".counter p").innerText = count;
-
-
-
-
-
-
-
+var count = 0;
+var lowerbtn = document.querySelector(".lower-count");
+lowerbtn.addEventListener("click", () => {
+  count--;
+  document.querySelector(".counter p").innerText = count;
+  if (count == 0) {
+    lowerbtn.disabled = true;
+    lowerbtn.classList.add("opacButton");
+  }
+});
+document.querySelector(".add-count").addEventListener("click", () => {
+  if (count >= 0) {
+    lowerbtn.disabled = false;
+    lowerbtn.classList.remove("opacButton");
+  }
+  count++;
+  document.querySelector(".counter p").innerText = count;
+});
 
 // Selecting Required Buttons to Count
-const buttons = Array.from(document.querySelectorAll('button'));
-let countContainer = document.querySelector('.counter p');
+// const buttons = Array.from(document.querySelectorAll('button'));
+// let countContainer = document.querySelector('.counter p');
 
-buttons.forEach((button) => {
-	button.addEventListener('click', (e) => {
-		if(e.target.classList.contains('add-count')) {
-			countContainer.innerText++;
-		} else if(e.target.classList.contains('lower-count')) {
-			if(countContainer.innerText == 0) {
-				e.preventDefault();
-			} else {
-				countContainer.innerText --;
-			}
-		}
-})
-})
-
-
-
-
-
-
-
-
-
-
-
+// buttons.forEach((button) => {
+// 	button.addEventListener('click', (e) => {
+// 		if(e.target.classList.contains('add-count')) {
+// 			countContainer.innerText++;
+// 		} else if(e.target.classList.contains('lower-count')) {
+// 			if(countContainer.innerText == 0) {
+// 				e.preventDefault();
+// 			} else {
+// 				countContainer.innerText --;
+// 			}
+// 		}
+// })
+// })
